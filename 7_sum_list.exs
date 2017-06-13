c7d291a9074b6dbbd0e9cf16b785318fa5cbd9ed
mehdivk <valikhani@sirangweb.com>
2017-06-13 20:53:20 +1000

    used private functions for _sum

--- 7_sum_list.exs
+++ 7_sum_list.exs
@@ -1,7 +1,7 @@
 defmodule ListOps do
   def sum(list), do: _sum(list, 0)
-  def _sum([], total), do: total
-  def _sum([head | tail], total), do: _sum(tail, total + head)
+  defp _sum([], total), do: total
+  defp _sum([head | tail], total), do: _sum(tail, total + head)
 end
 
 
