9365faddaf2fbd2478f5ae220dcadb57d08014d3
mehdivk <hi@mv.id.au>
2017-06-15 09:58:20 +1000

    get_and_update_in using Access.all()

--- maps/access1.exs
+++ maps/access1.exs
@@ -19,3 +19,7 @@ cast = [
 
 IO.inspect get_in(cast, [Access.all(), :character])
 IO.inspect get_in(cast, [Access.at(1), :role])
+
+
+IO.inspect get_and_update_in(cast, [Access.all(), :actor, :last],
+    fn (val) -> {val, String.upcase(val)} end)
