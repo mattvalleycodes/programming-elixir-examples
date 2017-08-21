527ea0836c2be59b36a68529c1078ba06003e496
mehdivk <hi@mv.id.au>
2017-08-21 14:33:01 +0430

    implemented is_collection? using protocol

---
+++ protocol/is_collection.exs
@@ -0,0 +1,30 @@
+defprotocol Collection do
+
+  @fallback_to_any true
+  def is_collection?(value)
+
+end
+
+
+defimpl Collection, for: [List, Tuple, BitString, Map] do
+  def is_collection?(_), do: true
+end
+
+
+defimpl Collection, for: Any do
+  def is_collection?(_), do: false
+end
+
+
+items = [
+  1,
+  1.0,
+  [1, 2],
+  {1, 2},
+  %{},
+  "cat",
+]
+
+Enum.each items, fn value ->
+  IO.puts "#{inspect(value)}: #{Collection.is_collection?(value)}"
+end
