6cad78e30a304fd7a200a88c9c6b7a4cfe84068e
mehdivk <hi@mv.id.au>
2017-08-21 19:00:17 +0430

    implemented Enumerable.member? for Bitmap

--- protocol/bitmap_enumerable.exs
+++ protocol/bitmap_enumerable.exs
@@ -4,4 +4,9 @@ defimpl Enumerable, for: Bitmap do
   def count(%Bitmap{value: value}) do
     { :ok, trunc(log(abs(value))/log(2)) + 1 }
   end
+
+
+  def member?(value, bit_number) do
+    { :ok, 0 <= bit_number && bit_number < Enum.count(value)}
+  end
 end
